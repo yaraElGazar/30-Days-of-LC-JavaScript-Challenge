8ff59509a806652f127a597cfef13591f71cd79a
yaraElGazar <yara_elgazar@outlook.com>
2023-05-19 18:49:29 +0300

    day 5 added

---
+++ day5.js
@@ -0,0 +1,22 @@
+// 2634. Filter Elements from Array
+
+// Given an integer array arr and a filtering function fn, return a new array with a fewer or equal number of elements.
+
+// The returned array should only contain elements where fn(arr[i], i) evaluated to a truthy value.
+
+// Please solve it without the built-in Array.filter method.
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+  let filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      filteredArr.push(arr[i]);
+    }
+  }
+  return filteredArr;
+};
