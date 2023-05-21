54b44a7ccc36f78147b816a551236c3bc2fc5163
yaraElGazar <yara_elgazar@outlook.com>
2023-05-21 06:49:32 +0300

    day 12 added

---
+++ day12.js
@@ -0,0 +1,28 @@
+// 2637. Promise Time Limit
+
+// Given an asyncronous function fn and a time t in milliseconds, return a new time limited version of the input function.
+
+// A time limited function is a function that is identical to the original unless it takes longer than t milliseconds to fullfill. In that case, it will reject with "Time Limit Exceeded".  Note that it should reject with a string, not an Error.
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+  return async function (...args) {
+    return await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject("Time Limit Exceeded");
+      }, t);
+      fn(...args)
+        .then(resolve)
+        .catch(reject);
+    });
+  };
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
